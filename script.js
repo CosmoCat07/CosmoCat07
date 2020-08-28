d452ace9a490c3053d7a6e5fe92b5e657a7cbc69
E S <eitanls19@gmail.com>
2020-08-28 01:13:31 +0000

    Completed Navbar Functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+var menu = document.getElementById("home-menubar");
+var homebar = document.getElementById("home-navbar");
+var hidden = true
+
+function navbar() {
+  if (hidden == true) {
+    homebar.style.visibility = "visible";
+    hidden = false;
+  } else {
+    homebar.style.visibility = "hidden";
+    hidden = true;
+  }
+};
