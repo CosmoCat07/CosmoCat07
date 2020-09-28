53f061e5fc91aeca75b4b02825e1a144c22e4344
CosmoCat07 <eitanls19@gmail.com>
2020-09-28 14:28:49 +0000

    Some Major js Edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var homebar = document.getElementById("home-navbar");
+var homebar = document.getElementById("homeNavbar");
 var hidden = true
 
 function navbar() {
@@ -8,5 +8,87 @@ function navbar() {
   } else {
     homebar.style.visibility = "hidden";
     hidden = true;
+  }
+};
+
+var navbars = document.getElementsByClassName("navbar");
+
+var nav = {
+  icon : function() {
+    var menuIcon = document.createElement("div");
+    menuIcon.className = "menuicon";
+    menuIcon.onclick = navbar;
+    for (i = 0; i < 3; i++) {
+      var menuBar = document.createElement("div");
+      menuBar.className = "menubar";
+      menuBar.innerHTML = "1";
+      menuIcon.appendChild(menuBar);
+    };
+    document.body.insertBefore(menuIcon,document.body.childNodes[0]);
+  },
+  menu : function(id) {
+    nav.icon();
+    var bar = document.getElementById(id);
+    this.addButton(bar,"Home","index.html");
+    this.addButton(bar,"About","about.html");
+    this.addButton(bar,"My Streams","twitch.html");
+    this.addButton(bar,"My Setup","setup.html");
+  },
+  addNavLink : function(text, page) {
+    var link = document.createElement("a");
+    link.className = "navlink";
+    link.href = page;
+    var textNode = document.createTextNode(text);
+    link.appendChild(textNode);
+    link.fontSize = "medium";
+    return link;
+  },
+  addNavItem : function(text, page) {
+    var button = document.createElement("li");
+    button.className = "navitem";
+    button.appendChild(this.addNavLink(text,page));
+    resize.check(button);
+    return button;
+  },
+  addButton: function(bar, text, page) {
+    bar.appendChild(this.addNavItem(text,page));
+  }
+};
+
+var resize = {
+  check : function(item) {
+    if (window.innerWidth <= 600 && item.className == "navitem") {
+      this.smallButton(item);
+    } else if (window.innerWidth <= 600 && item == document.getElementById("aboutText")) {
+      this.smallAbout();
+    } else if (window.innerWidth <= 600) {
+      this.small(item);
+    }
+  },
+  smallButton : function(item) {
+    item.style.width = "92px";
+    var text = item.childNodes[0].style;
+    text.fontSize = "small";
+    return item;
+  },
+  small : function(item) {
+    var text = item.childNodes[0].style;
+    text.fontSize = "small";
+    return item;
+  },
+  smallAbout : function() {
+    var about = document.getElementById("aboutText");
+    about.style.left = "5%";
+    about.style.width = "90%";
+    var text = about.childNodes[0];
+    text.style.fontSize = "large";
+  }
+};
+
+var load = {
+  about : function() {
+    nav.menu("aboutNavbar");
+    var about = document.getElementById("aboutText");
+    resize.check(about);
   }
 };
